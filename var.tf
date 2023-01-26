84472d0f822bb9619c44129cd8b1338c914fdc38
fatimatabassum1 <121183698+fatimatabassum1@users.noreply.github.com>
2023-01-26 13:45:42 +0530

    var.tf file

--- var.tf
+++ var.tf
@@ -1,19 +1,20 @@
 variable "region" {
-   
+   default = "ap-south-1"
 }
 
 variable "ami-id" {
-    
+    description = "ami is for ap-south-1 region"
+    default = "ami-0ef82eeba2c7a0eeb"
 }
 
 variable "instance-type" {
-   
+    default = "t2.micro"
 }
 
 variable "key-name" {
-    
+    default = "terraform1"
 }
 
 variable "tag-name" {
-   
+    default = "terraform-ec2"
 }
